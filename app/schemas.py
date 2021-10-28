646546462781f72f80d1a1799bc3fbd1a0ee0c14
Olivier Cervello <ocervello@google.com>
2021-10-28 16:15:53 +0200

    feat: update schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,28 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import Optional
 
 from pydantic import BaseModel
 
 
-class Event(BaseModel):
+class EventBase(BaseModel):
     event_type: str
     timestamp: datetime = datetime.utcnow()
     user_id: Optional[str]
-    device_id: Optional[str]
     barcode: Optional[str]
     points: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class EventCreate(EventBase):
+    device_id: Optional[str]
+
+
+class BadgeBase(BaseModel):
+    user_id: Optional[str]
+    badge_name: str
+    level: int
 
     class Config:
         orm_mode = True
