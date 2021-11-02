61d5c2ddf59475cd85eb022b3161501990fdd957
autoblack <actions@github.com>
2021-11-02 16:45:23 +0000

    fixup: Format Python code with Black

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -6,14 +6,14 @@ import os
 
 client = TestClient(app)
 
-ADMIN_USERNAME = os.environ['ADMIN_USERNAME']
-ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
+ADMIN_USERNAME = os.environ["ADMIN_USERNAME"]
+ADMIN_PASSWORD = os.environ["ADMIN_PASSWORD"]
 TEST_EVENT = {
-    'user_id': 'test',
-    'event_type': 'invite_shared',
-    'device_id': '1A2B-2B3C-3D4E-4F5G',
+    "user_id": "test",
+    "event_type": "invite_shared",
+    "device_id": "1A2B-2B3C-3D4E-4F5G",
 }
-TEST_EVENT_CFG = EVENT_TYPES[TEST_EVENT['event_type']]
+TEST_EVENT_CFG = EVENT_TYPES[TEST_EVENT["event_type"]]
 
 
 def test_get_docs():
@@ -26,9 +26,9 @@ def test_get_metrics():
     assert response.status_code == 200
 
 
-#-----------------#
+# -----------------#
 # EVENTS ENDPOINT #
-#-----------------#
+# -----------------#
 
 
 def test_get_events():
@@ -37,13 +37,13 @@ def test_get_events():
 
 
 def test_create_event():
-    response = client.post("/events",
-                           auth=(ADMIN_USERNAME, ADMIN_PASSWORD),
-                           json=TEST_EVENT)
+    response = client.post(
+        "/events", auth=(ADMIN_USERNAME, ADMIN_PASSWORD), json=TEST_EVENT
+    )
     assert response.status_code == 200
     json = response.json()
-    assert json['barcode'] == None
-    assert json['points'] == TEST_EVENT_CFG['points']
+    assert json["barcode"] == None
+    assert json["points"] == TEST_EVENT_CFG["points"]
 
 
 def test_create_event_unauthorized():
@@ -51,9 +51,9 @@ def test_create_event_unauthorized():
     assert response.status_code == 401
 
 
-#-------------#
+# -------------#
 # LEADERBOARD #
-#-------------#
+# -------------#
 
 
 def test_get_leaderboard():
@@ -62,5 +62,5 @@ def test_get_leaderboard():
 
 
 def test_get_badges():
-    response = client.get('/badges')
+    response = client.get("/badges")
     assert response.status_code == 200
