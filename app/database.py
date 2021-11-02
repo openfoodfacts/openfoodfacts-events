75a2852dab69a1d571cd58144ec5c790a64e118c
Olivier Cervello <ocervello@google.com>
2021-11-02 15:09:39 +0100

    Run black on code

--- app/database.py
+++ app/database.py
@@ -6,8 +6,9 @@ import time
 import logging
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URI",
-                                         "sqlite:///./sql_app.db")
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI", "sqlite:///./sql_app.db"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -20,15 +21,13 @@ logger.setLevel(logging.INFO)
 
 
 @event.listens_for(Engine, "before_cursor_execute")
-def before_cursor_execute(conn, cursor, statement, parameters, context,
-                          executemany):
-    conn.info.setdefault('query_start_time', []).append(time.time())
+def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    conn.info.setdefault("query_start_time", []).append(time.time())
     logger.debug("Start Query: %s", statement)
 
 
 @event.listens_for(Engine, "after_cursor_execute")
-def after_cursor_execute(conn, cursor, statement, parameters, context,
-                         executemany):
-    total = time.time() - conn.info['query_start_time'].pop(-1)
+def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    total = time.time() - conn.info["query_start_time"].pop(-1)
     logger.debug("Query Complete!")
     logger.debug("Total Time: %f", total)
